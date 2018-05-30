60dfdc9e0b2374e01cc67f9947ee4c95582f8ba2
Renata <rzjunges@inf.ufpel.edu.br>
2018-05-30 13:13:54 -0300

    Read ground truth file and jaccard index of gt and pred image

--- HOG + SVM/HOG_LinearSVC-testing.py
+++ HOG + SVM/HOG_LinearSVC-testing.py
@@ -19,7 +19,7 @@ from skimage.feature import hog
 from sklearn.metrics import jaccard_similarity_score
 from sklearn.svm import LinearSVC
 from imutils import paths
-from helpers import mask_hsv_red, circle_values, draw_circle, rectangle_coord
+from helpers import mask_hsv_red, circle_values, draw_circle, rectangle_coord, image_Jaccard
 
 #Argparse
 ap = argparse.ArgumentParser()
@@ -35,7 +35,7 @@ list_pickle = open(modelFile, 'rb')
 model = pickle.load(list_pickle)
 list_pickle.close()
 
-#Test a model
+#Test a modelgt_file
 print("Testing a Model")
 padding = 3
 j = 0
@@ -44,51 +44,46 @@ width, height = args["dimension"]
 for file in glob.glob(args["testing"] + 'clip_i5s_0094*'): #Take all files (need to be video)
     print("The current video is {}".format(file.split('/')[-1]))
     cap = cv2.VideoCapture(file) #Capture the video
-    
+    file_name = file.split('/')[-1].split('.')[0] #File name without extensions 
+
     #To save a video with frames and rectangles in ROIs
     fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G') #codec
-    video_out = cv2.VideoWriter(file.split('/')[-1].split('.')[0] + '.avi', fourcc, 20.0, (1920, 1080))
+    video_out = cv2.VideoWriter(file_name + '.avi', fourcc, 20.0, (1920, 1080))
     frame_number = -1
 
     #Open ground truth file 
+    file_GT = open(file_name, "r")
+    line_GT = file_GT.readline()
     
-    gt_file = open("clip_i5s_0094_GT", "r")
-    line = gt_file.readline()
-
     while(cap.isOpened()):
         ret, frame = cap.read() #Capture frame-by-frame
         frame_number = frame_number + 1
-        flag_gt = False
+        flag_GT = False
+        
         #If the video has ended
         if not ret:
             break
 
         img = frame.copy()
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        #mask with the red color of the image
-        mask = mask_hsv_red(hsv)
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #Put img(frame) in grayscale
+        mask = mask_hsv_red(hsv) #mask with the red color of the image
 
-        ##########################################
-        #Ground Truth
-        if (line):
-            line = line.strip()
-            frame_gt = line.split(",")[0]  
-            if int(frame_gt) == frame_number:
-                frame_gt, x, y, width_gt, heigth_gt = line.split(",")
-                x, y, width_gt, heigth_gt = int(float(x)), int(float(y)), int(float(width_gt)), int(float(heigth_gt))
-                #x and y are the top left coord
-                x2 = x + width_gt
-                y2 = y + heigth_gt
-                #Take a rect of frame
-                rect_gt = img[y:y2, x:x2]
-                rect_gt = cv2.resize(rect_gt, (width, height)) #don't mantain the aspect ratio
+        #GROUND TRUTH
+        if (line_GT):
+            line_GT = line_GT.strip()
+            frame_GT = line_GT.split(",")[0] 
+
+            #if frame_GT is equal to frame_number then there is a traffic signal 
+            if int(frame_GT) == frame_number:
+                frame_GT, x, y, width_GT, heigth_GT = line_GT.split(",") #read all information of ground truth line
+                x1_GT, y1_GT, width_GT, heigth_GT = int(float(x)), int(float(y)), int(float(width_GT)), int(float(heigth_GT)) #convert string to int
                 
-                #cv2.imwrite('GT_rect/' + str(frame_gt) + ".jpg", rect_gt) 
-                flag_gt = True
-                #Read next gt
-                line = gt_file.readline()
-
-        ##########################################
+                #x1_GT and y1_GT are the top left coord, then the x2_GT e y2_GT are the bottom right coord
+                x2_GT = x1_GT + width_GT
+                y2_GT = y1_GT + heigth_GT 
+                
+                flag_GT = True #Flag true because there is a traffic signal in this frame
+                line_GT = file_GT.readline() #Read next ground truth line
         
         # HOUGH CIRCLE
         cimg = img.copy()
@@ -103,23 +98,25 @@ for file in glob.glob(args["testing"] + 'clip_i5s_0094*'): #Take all files (need
             #For each circle found
             for i in circles[0,:]:
                 x, y, radius = circle_values(i) 
-                j = j + 1 #This line is necessary to write mask (optional)
+                j = j + 1 #This line is to save images in disk
 
                 # draw the circle of ROI
                 #cimg = draw_circle (cimg, (x,y), radius)
 
                 #Points to draw/take rectangle in image 
-                x1, y1, x2, y2 = rectangle_coord((x,y), radius, padding, img.shape)
+                x1_PRED, y1_PRED, x2_PRED, y2_PRED = rectangle_coord((x,y), radius, padding, img.shape)
                
                 #cut image
-                rect = img[y1:y2, x1:x2].copy()  
+                rect = img[y1_PRED:y2_PRED, x1_PRED:x2_PRED].copy()  
                                 
                 #For each ROI (rect) resize to dimension and verify if fits in model
                 if rect.shape[0] > 0 and rect.shape[1] > 0: 
                     img_resize = cv2.resize(rect, (width, height)).copy()
+                else:
+                    continue
 
                 #Put in grayscale
-                img_gray = cv2.cvtColor(img_resize, cv2.COLOR_BGR2GRAY)
+                img_gray = cv2.cvtColor(img_resize, cv2.COLOR_BGR2GRAY).copy()
                 
                 #HOG method
                 (H, hogImage) = hog(img_gray, orientations=9, pixels_per_cell=(8,8), cells_per_block=(2,2), transform_sqrt=True, visualise=True, block_norm='L2-Hys')
@@ -131,10 +128,24 @@ for file in glob.glob(args["testing"] + 'clip_i5s_0094*'): #Take all files (need
                     continue
 
                 if (pred.title()).lower() == 'pos':
-                    #if flag_gt:
-                    #   print(jaccard_similarity_score(rect_gt.flatten(), img_resize.flatten()))
+                    if flag_GT:
+                        image_GT, image_Pred = image_Jaccard(x1_GT, y1_GT, x2_GT, y2_GT, x1_PRED, y1_PRED, x2_PRED, y2_PRED)
+                        JI = jaccard_similarity_score(image_GT.flatten(), image_Pred.flatten())
+                        '''
+                        x2_PRED = x1_PRED + width_GT
+                        y2_PRED = y1_PRED + heigth_GT
+
+                        image_GT = img[y1_GT:y2_GT, x1_GT:x2_GT].copy()
+                        image_Pred = img[y1_PRED:y2_PRED, x1_PRED:x2_PRED].copy()
+                        
+                        FINAL = np.hstack((image_GT, image_Pred))
+                        cv2.imwrite('Video_rect/' + str(j) + "image_GT.jpg", image_GT)
+                        cv2.imwrite('Video_rect/' + str(j) + "image_PRED.jpg", image_Pred)
+                        cv2.imwrite('Video_rect/' + str(j) + "Final.jpg", FINAL)
+                        '''
+                        print(jaccard_similarity_score(image_GT.flatten(), image_Pred.flatten()), JI)
                     draw_circle (img, (x,y), radius)
-                    cv2.rectangle(img, (x1,y1), (x2,y2), (0,0,255), 2)
+                    cv2.rectangle(img, (x1_PRED,y1_PRED), (x2_PRED,y2_PRED), (0,0,255), 2)
                         
                     #cv2.imwrite('Video_rect/PosResult/' + str(j) + ".jpg", img_resize) #Write Positive samples
                 #To write/save Negative samples uncomment the following lines
@@ -144,6 +155,6 @@ for file in glob.glob(args["testing"] + 'clip_i5s_0094*'): #Take all files (need
                 #cv2.imwrite('Mask/mask' + str(j) +'.jpg', mask)        
         video_out.write(img)
 
-    gt_file.close() #close ground truth file
+    file_GT.close() #close ground truth file
     cap.release() #Release the capture
     video_out.release() #Release the video writer
