b0b5d45df0de898fba9907a5afd53ec3b9c391fb
Renata <rzjunges@inf.ufpel.edu.br>
2018-06-23 18:05:48 -0300

    -ml correction

--- HOG + SVM/train.py
+++ HOG + SVM/train.py
@@ -14,6 +14,7 @@ Python Version: 3.6
 #python train.py -t ../../proibido_ultrapassar/neg_pos_48x48/ 
 #python train.py -t ../../60km_h/neg_pos_48x48/ -mf training_60km.dat
 #python train.py -t ../../80km_h/neg_pos_48x48/ -mf training_80km.dat
+#python train.py -t ../../datasets/digits/ -mf digits.dat -ml 2
 
 import numpy as np 
 import pickle
@@ -41,7 +42,7 @@ for file in paths.list_images(args["training"]):
     
     img_original = cv2.imread(file) 
     img = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY) #Image in grayscale
-
+    
     #Hog method
     (H, hogImage) = hog(img, orientations=9, pixels_per_cell=(8,8), cells_per_block=(2,2), transform_sqrt=True, visualise=True, block_norm='L2-Hys')
 
@@ -54,7 +55,7 @@ for file in paths.list_images(args["training"]):
 mode = args["machineLearning"]
 
 #Training a model
-if (mode == 2):
+if (int(mode) == 2):
     print("Training a Model with SVC...")
     model = svm.SVC()
 else:
@@ -67,7 +68,7 @@ model.fit(data,labels)
 #Saving the model
 directory = "../Models/"
 modelFile = args['modelFile']
-print("Saving the model in {}".format(modelFile))
+print("Saving the model in {}".format(directory + modelFile))
 
 if not os.path.exists(directory):
     #create the directory if it does not exist
