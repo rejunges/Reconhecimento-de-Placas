963c29a9e49bf944e8e6562a093ad0afd65595ab
Renata <rzjunges@inf.ufpel.edu.br>
2018-10-17 22:43:21 -0300

    Remocao das linhas com largura ou altura inferior a 40

--- Scripts/script_GT.py
+++ Scripts/script_GT.py
@@ -1,13 +1,13 @@
 """
 File name: script_GT.py
-Description: This file rewrites the GT file, only mantaining the pictures with dimensions 48x48 or superior
+Description: This file rewrites the GT file, only mantaining the pictures with dimensions 40x40 or superior
 Author: Renata Zottis Junges
 Python Version: 3.6
 """
 
 import glob
 
-for file in glob.glob("*GT.txt"): #Here put the name of ground truth file 
+for file in glob.glob("*GT*.txt"): #Here put the name of ground truth file 
     print("File: " + file + "\nTo: " + file.split(".")[0])
     #Open the GT file 
     with open(file, 'r') as arq: 
@@ -17,8 +17,8 @@ for file in glob.glob("*GT.txt"): #Here put the name of ground truth file
              
             #Read line by line until the end of file
             while(line):
-                frame, x, y, width, heigth = line.split(",")
-                if float(width) >= 48 and float(heigth) >= 48:
+                frame, x, y, width, heigth, _ = line.split(",")
+                if float(width) >= 40 and float(heigth) >= 40:
                     fw.write(line)
                 line = arq.readline()
         fw.close() 
